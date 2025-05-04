1cefd6c86aafe646d4d38d7b933e00fd2f34efe5
Harshini Gullapally <gullapallyharshini43@gmail.com>
2025-05-03 22:09:34 -0400

    Update app.py

--- project/app.py
+++ project/app.py
@@ -111,8 +111,9 @@ if st.session_state["page"] == "wishlist":
 stock_notifications()
 
 # ✅ Sidebar Navigation
+st.sidebar.header("Navigation")
+
 if selected_tab == "Stock Dashboard":
-    st.sidebar.header("Navigation")
     selected_tab = st.sidebar.radio("Select a section:", [
     "Stock Analysis", "Nasdaq Stock Visualization", "News Update", "Stock Assistant",
     "Quarterly Analysis", "Sector heatmap", "Intrinsic Value", "Stock Screener",
